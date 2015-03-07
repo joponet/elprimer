88b8c17c9445d56a0a772afa2b0391c890685ecc
joponet <joponet@gmail.com>
2015-03-07 19:21:20 +0100

    Main window

--- src/prova/Main.java
+++ src/prova/Main.java
@@ -1,8 +1,16 @@
 package prova;
 
+import java.awt.Frame;
+import java.awt.Label;
+
 public class Main {
 	
 	public void start(){
 		System.out.print("hola");
+		Frame window = new Frame("El Primer");
+		Label label = new Label("Hola tio!");
+		window.add(label);
+		window.setVisible(true);
+		
 	}
 }
