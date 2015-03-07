ffd97b8748c64a68ae8b414a42ab84bd0bd8c300
joponet <joponet@gmail.com>
2015-03-07 21:52:13 +0100

    linies en blanc

--- src/prova/Main.java
+++ src/prova/Main.java
@@ -26,39 +26,27 @@ public class Main {
 		window.addWindowListener(new WindowListener() {
 			
 			public void windowClosed(WindowEvent arg0) {
-		
 			}
 
 			public void windowActivated(WindowEvent arg0) {
-				
-				
 			}
 
 			public void windowClosing(WindowEvent arg0) {
 				System.exit(0);
 			}
-
 		
 			public void windowDeactivated(WindowEvent arg0) {
-								
 			}
 
 		
 			public void windowDeiconified(WindowEvent arg0) {
-			
-				
 			}
 
-		
 			public void windowIconified(WindowEvent arg0) {
-			
-				
 			}
 
 		
 			public void windowOpened(WindowEvent arg0) {
-			
-				
 			}
 		});
 		window.pack();
